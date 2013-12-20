0ea1b52c7d1a11e01b6fa07899ad8b1d27230b1b
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2013-12-19 22:37:42 -0800

    documentation for ~StoreClient() methods

--- src/main/java/org/springframework/social/evernote/api/Evernote.java
+++ src/main/java/org/springframework/social/evernote/api/Evernote.java
@@ -18,16 +18,55 @@ public interface Evernote extends ApiBinding {
 	UserStoreOperations userStoreClientOperations();
 
 
-	// to retrieve actual store client instances
-
+	/**
+	 * Returns underlying {@link ClientFactory} instance.
+	 *
+	 * @return {@link ClientFactory} instance.
+	 */
 	ClientFactory clientFactory();
 
-	BusinessNoteStoreClient businessNoteStoreClient();
+	/**
+	 * Retrieve {@link BusinessNoteStoreClient} instance from underlying {@link ClientFactory}.
+	 * <p/>
+	 * When checked exception has thrown while creating {@link BusinessNoteStoreClient}, it will be converted
+	 * to {@link EvernoteException} which is an unchecked exception.
+	 *
+	 * @return {@link BusinessNoteStoreClient} instance.
+	 * @throws EvernoteException encapsulating thrown checked exception.
+	 */
+	BusinessNoteStoreClient businessNoteStoreClient() throws EvernoteException;
 
-	LinkedNoteStoreClient linkedNoteStoreClient(LinkedNotebook linkedNotebook);
+	/**
+	 * Retrieve {@link LinkedNoteStoreClient} instance from underlying {@link ClientFactory}.
+	 * <p/>
+	 * When checked exception has thrown while creating {@link LinkedNoteStoreClient}, it will be converted
+	 * to {@link EvernoteException} which is an unchecked exception.
+	 *
+	 * @return {@link LinkedNoteStoreClient} instance.
+	 * @throws EvernoteException encapsulating thrown checked exception.
+	 */
+	LinkedNoteStoreClient linkedNoteStoreClient(LinkedNotebook linkedNotebook) throws EvernoteException;
 
-	NoteStoreClient noteStoreClient();
+	/**
+	 * Retrieve {@link NoteStoreClient} instance from underlying {@link ClientFactory}.
+	 * <p/>
+	 * When checked exception has thrown while creating {@link NoteStoreClient}, it will be converted
+	 * to {@link EvernoteException} which is an unchecked exception.
+	 *
+	 * @return {@link NoteStoreClient} instance.
+	 * @throws EvernoteException encapsulating thrown checked exception.
+	 */
+	NoteStoreClient noteStoreClient() throws EvernoteException;
 
-	UserStoreClient userStoreClient();
+	/**
+	 * Retrieve {@link UserStoreClient} instance from underlying {@link ClientFactory}.
+	 * <p/>
+	 * When checked exception has thrown while creating {@link UserStoreClient}, it will be converted
+	 * to {@link EvernoteException} which is an unchecked exception.
+	 *
+	 * @return {@link UserStoreClient} instance.
+	 * @throws EvernoteException encapsulating thrown checked exception.
+	 */
+	UserStoreClient userStoreClient() throws EvernoteException;
 
 }
